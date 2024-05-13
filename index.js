1d9797009db17a5eb461b1056a6e0fa84b910675
Satyadarsan senapati <satyasenapati58@gmail.com>
2024-05-13 22:14:56 +0530

    version 1.0.1

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 // myLib.js
 
 const MyLib = {
-    scrollTrigger: function(element, animation, options) {
-      // Implementation for scroll-triggered animation
-    },
-    
-    stagger: function(elements, animation, delay, options) {
-      // Implementation for staggered animation
-    },
-    
-    on: function(event, element, animation, options) {
-      // Implementation for event-triggered animation
-    },
-    
-    before: function(animation, action) {
-      // Implementation for action before animation
-    },
-    
-    after: function(animation, action) {
-      // Implementation for action after animation
-    },
-    
-    animate: function(element, animation, customCSS) {
-      // Implementation for animation
-    }
-  };
+  scrollTrigger: function(element, animation, options) {
+      window.addEventListener('scroll', () => {
+          const elementRect = element.getBoundingClientRect();
+          const viewportHeight = window.innerHeight;
+          
+          if (elementRect.top < viewportHeight) {
+              element.classList.add(animation);
+          } else {
+              element.classList.remove(animation);
+          }
+      });
+  },
   
-  // Export MyLib for use in other modules
-  module.exports = MyLib;
+  stagger: function(elements, animation, delay, options) {
+      elements.forEach((element, index) => {
+          setTimeout(() => {
+              element.classList.add(animation);
+          }, index * delay);
+      });
+  },
   
+  on: function(event, element, animation, options) {
+      element.addEventListener(event, () => {
+          element.classList.add(animation);
+      });
+  },
+  
+  before: function(animation, action) {
+      action();
+      setTimeout(() => {
+          action.classList.add(animation);
+      }, 0);
+  },
+  
+  after: function() {
+      // Logic to execute after animation
+  },
+  
+  animate: function(element, animation, customCSS) {
+      element.style.animation = `${animation} ${customCSS.duration} ${customCSS.timingFunction} ${customCSS.delay} ${customCSS.iterationCount} ${customCSS.direction} ${customCSS.fillMode} ${customCSS.playState}`;
+  },
+
+  customAnimation: function(element, keyframes, options) {
+      const animation = element.animate(keyframes, options);
+      return animation;
+  },
+  test:function(){
+    console.log("test")
+  }
+};
+
+module.exports = MyLib;
