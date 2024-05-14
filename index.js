ee35a355f54a42b6f6acafb2242bc41355ab48f1
Saty19 <satyasenapati58@gmail.com>
2024-05-14 12:11:48 +0530

    modify-testing

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
-// myLib.js
 
-const MyLib = {
-  scrollTrigger: function(element, animation, options) {
-      window.addEventListener('scroll', () => {
-          const elementRect = element.getBoundingClientRect();
-          const viewportHeight = window.innerHeight;
-          
-          if (elementRect.top < viewportHeight) {
-              element.classList.add(animation);
-          } else {
-              element.classList.remove(animation);
-          }
-      });
-  },
+import { TextScramble ,ShuffleTextEffect} from "./text-animate";
+  const SoCi = {
+    
+    scrambleText: function(el, newText, eventType) {
+        const element=document.querySelector(el);
+        const fx = new TextScramble(element);
+        const eventListener = () => fx.setText(newText);
+        element.addEventListener(eventType, eventListener);
+        return eventListener;
+      },
+      shuffleText: function(el, newText, eventType) {
+        const elem=document.querySelector(el);
+       const fx= new ShuffleTextEffect(elem, newText, eventType);
+    },
+    
+    test:function(){
+      console.log("test");
+    }
+  };
   
-  stagger: function(elements, animation, delay, options) {
-      elements.forEach((element, index) => {
-          setTimeout(() => {
-              element.classList.add(animation);
-          }, index * delay);
-      });
-  },
+  export default SoCi;
   
-  on: function(event, element, animation, options) {
-      element.addEventListener(event, () => {
-          element.classList.add(animation);
-      });
-  },
-  
-  before: function(animation, action) {
-      action();
-      setTimeout(() => {
-          action.classList.add(animation);
-      }, 0);
-  },
-  
-  after: function() {
-      // Logic to execute after animation
-  },
-  
-  animate: function(element, animation, customCSS) {
-      element.style.animation = `${animation} ${customCSS.duration} ${customCSS.timingFunction} ${customCSS.delay} ${customCSS.iterationCount} ${customCSS.direction} ${customCSS.fillMode} ${customCSS.playState}`;
-  },
-
-  customAnimation: function(element, keyframes, options) {
-      const animation = element.animate(keyframes, options);
-      return animation;
-  },
-  test:function(){
-    console.log("test")
-  }
-};
-
-module.exports = MyLib;
